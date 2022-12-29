7b1673700117e243a8cc409595641652a53eae21
Jason DeSante <jasondesante@hotmail.com>
2022-12-29 06:36:01 -0500

    enabled cors for all routes

--- src/index.ts
+++ src/index.ts
@@ -18,11 +18,11 @@ app.use(express.json());
 
 
 // Enable CORS for all routes
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(cors());
