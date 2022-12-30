87dea789af4d22c1647741dca71d3a2dc2955ed4
Jason DeSante <jasondesante@hotmail.com>
2022-12-30 07:37:55 -0500

    more cors tests

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,21 @@ app.use(express.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", 'PUT,POST,GET,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+
+  origin: "*",
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: true,
+  optionsSuccessStatus: 204
+
+}));
 
 
 app.use(`/server`, parseServer.app);
