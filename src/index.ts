d5247daeba2f0441c522480b44f61c53f266fac7
Jason DeSante <jasondesante@hotmail.com>
2022-12-30 07:51:57 -0500

    another one

--- src/index.ts
+++ src/index.ts
@@ -20,24 +20,22 @@ app.use(express.json());
 
 
 // Enable CORS for all routes
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", 'PUT,POST,GET,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", 'PUT,POST,GET,DELETE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 
-// app.use(cors());
-app.use(cors({
-
-  origin: "*",
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: true,
-  optionsSuccessStatus: 204
-
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: "*",
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   preflightContinue: true,
+//   optionsSuccessStatus: 204
+// }));
 
 
 app.use(`/server`, parseServer.app);
